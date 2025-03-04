3b4c0d7c4a70a90470ee47791935a0dc64c9f6bb
Hira Jabeen <142384028+hirajabeen9@users.noreply.github.com>
2025-03-04 23:50:16 +0500

    Greetings added

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	fmt.Println("Welcome to", conferenceName, "the booking app!")
+	fmt.Println("Get your", conferenceName, " tickets now!")
+	fmt.Println("Only", conferenceTickets, " tickets avaiable!")
+
 }
