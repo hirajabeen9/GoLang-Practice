1525db77f6d3d5c4271ebdd55cc56629b0eef52f
hirajabeen9 <hirajabeen788@gmail.com>
2025-03-15 18:58:54 +0500

    data types , array and slice

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,33 +5,37 @@ package main
 import "fmt"
 
 func main() {
-	// Declaring a string variable using the var keyword.
-	var conferenceName = "Go Conference"
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	//var bookings [50]string //array
+	var bookings []string //slice
 
-	// Declaring a constant using the const keyword.
-	// Constants are immutable and cannot be changed after declaration.
-	const conferenceTickets = 50
-
-	// Declaring a variable using the var keyword.
-	// Variables declared with var can be reassigned later.
-	var remainingTickets = conferenceTickets - 10
-
-	// Printing a welcome message using fmt.Println, which adds a newline at the end.
+	//fmt.Printf("conferenceName: %T conferenceTickets: %T  remainingTickets: %T\n", conferenceName, conferenceTickets, remainingTickets)
 	fmt.Println("Welcome to", conferenceName, "the booking app!")
-
-	// Printing a formatted string using fmt.Printf.
-	// %v is a placeholder that gets replaced with the corresponding variable value.
 	fmt.Printf("Get your %v tickets now! Only %v tickets are available!\n", conferenceName, conferenceTickets)
-
-	// Printing the number of remaining tickets.
 	fmt.Println(remainingTickets, "Remaining tickets!")
 
 	var userName string
-	var userBuyTickets int
+	var userBuyTickets uint
+	var email string
+
 	fmt.Println("Enter your name:")
 	fmt.Scanln(&userName)
+	fmt.Println("Enter your active email:")
+	fmt.Scanln(&email)
 	fmt.Println("Enter the number of tickets you want to buy:")
 	fmt.Scanln(&userBuyTickets)
-	fmt.Printf("Thank you %v for booking %v tickets for %v!\n", userName, userBuyTickets, conferenceName)
+
+	remainingTickets = remainingTickets - userBuyTickets
+
+	//bookings[0] = userName + " " + email
+	bookings = append(bookings, userName+" "+email)
+	fmt.Printf("Thank you %v for booking %v tickets for %v. you will receive confirmation details at this email %v \n", userName, userBuyTickets, conferenceName, email)
+	fmt.Println("Be Quick!", remainingTickets, "tickets are remaining !")
+	fmt.Printf("The Whole Array %v \n", bookings)
+	fmt.Printf("The First Array %v \n", bookings[0])
+	fmt.Printf("The Array Type %T \n", bookings)
+	fmt.Printf("The Array Length/Size %v \n", len(bookings))
 
 }
